34d4b83cb25afc1dbe88d3635cde5beab06fe9e6
kamalakannanngk <111722165+kamalakannanngk@users.noreply.github.com>
2023-09-05 21:19:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,22 +16,6 @@ const battery = () => {
         updateLevelChange();
       }
       updateAll();
-      function secondsToHoursMinutes(seconds) {
-        // Calculate hours and remaining seconds
-        const hours = Math.floor(seconds / 3600);
-        const remainingSeconds = seconds % 3600;
-
-        // Calculate minutes
-        const minutes = Math.floor(remainingSeconds / 60);
-
-        return { hours, minutes };
-      }
-
-      // Example usage:
-      const totalSeconds = 7200; // Replace with the number of seconds you want to convert
-      const { hours, minutes } = secondsToHoursMinutes(totalSeconds);
-
-      console.log(`${hours} hour(s) ${minutes} minute(s)`);
 
       // Battery Level
       battery.addEventListener("chargingchange", () => {
@@ -55,8 +39,7 @@ const battery = () => {
         updateBatteryChargingTime();
       });
       function updateBatteryChargingTime() {
-        const seconds1 = battery.chargingTime;
-        secondsToHoursMinutes(seconds1);
+        batteryChargingTime.innerHTML = battery.chargingTime + " seconds";
       }
 
       // Battery DisCharging Time
@@ -64,8 +47,6 @@ const battery = () => {
         updateBatteryDisChargingTime();
       });
       function updateBatteryDisChargingTime() {
-        const seconds2 = battery.chargingTime;
-        secondsToHoursMinutes(seconds2);
         batteryDisChargingTime.innerHTML = battery.dischargingTime + " seconds";
       }
       //Battery level change
